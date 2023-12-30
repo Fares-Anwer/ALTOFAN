b01119f6b317a6f55f0ca9a81bed37d7ced55839
Abdulwhahed-fouad-Alhetar <143293809+Abdulwhahed-fouad-Alhetar@users.noreply.github.com>
2023-12-31 00:56:13 +0300

    no thing

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="">
       <meta name="author" content="">
-      <title>101+ News | Home Page</title>
+      <title>ALTOFAN</title>
       <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
       <!-- Custom styles for this template -->
