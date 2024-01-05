1935ab32a18164bd0eb3a596cff5e79fbc26fa86
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2024-01-04 16:12:06 -0800

    correct error which show when show catigory

--- category.php
+++ category.php
@@ -45,17 +45,16 @@ include('includes/config.php');
                             <div class="col-lg-12">
                                 <ul class="list-unstyled mb-0">
                                     <?php
-                           $stmt = $con->prepare("select id,CategoryName from tblcategory");
-                           $stmt->execute();
-                           if ($stmt->rowCount()) {
-                              foreach ($stmt->fetchAll() as $row) {
-                           ?>
-                                    <li class=" mb-2">
-                                        <a href="category.php?catid=<?php echo htmlentities($row['id']) ?>"
-                                            class="text-secondary"><?php echo htmlentities($row['CategoryName']); ?></a>
-                                    </li>
+                                    $stmt = $con->prepare("select id,CategoryName from tblcategory");
+                                    $stmt->execute();
+                                    if ($stmt->rowCount()) {
+                                        foreach ($stmt->fetchAll() as $row) {
+                                    ?>
+                                            <li class=" mb-2">
+                                                <a href="category.php?catid=<?php echo htmlentities($row['id']) ?>" class="text-secondary"><?php echo htmlentities($row['CategoryName']); ?></a>
+                                            </li>
                                     <?php }
-                           } ?>
+                                    } ?>
                                 </ul>
                             </div>
                         </div>
@@ -67,9 +66,9 @@ include('includes/config.php');
 
                 <!-- Blog Post -->
                 <?php
-            if ($_GET['catid'] != '') {
-               $_SESSION['catid'] = intval($_GET['catid']);
-            }
+                if ($_GET['catid'] != '') {
+                    $_SESSION['catid'] = intval($_GET['catid']);
+                }
 
 
 
@@ -77,95 +76,73 @@ include('includes/config.php');
 
 
 
-            if (isset($_GET['pageno'])) {
-               $pageno = $_GET['pageno'];
-            } else {
-               $pageno = 1;
-            }
-            $no_of_records_per_page = 8;
-            $offset = ($pageno - 1) * $no_of_records_per_page;
+                if (isset($_GET['pageno'])) {
+                    $pageno = $_GET['pageno'];
+                } else {
+                    $pageno = 1;
+                }
+                $no_of_records_per_page = 8;
+                $offset = ($pageno - 1) * $no_of_records_per_page;
 
 
-            $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
-            $result = $con->prepare($total_pages_sql);
-            $total_rows = $result->rowCount();
-            $total_pages = ceil($total_rows / $no_of_records_per_page);
+                $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
+                $result = $con->prepare($total_pages_sql);
+                $total_rows = $result->rowCount();
+                $total_pages = ceil($total_rows / $no_of_records_per_page);
 
 
-            $stmt = $con->prepare("select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.CategoryId='" . $_SESSION['catid'] . "' and tblposts.Is_Active=1 order by tblposts.id desc LIMIT $offset, $no_of_records_per_page");
-            $stmt->execute();
-            if ($stmt->rowCount() == 0) { {
-                  echo "No record found";
-               }
-            } else {
-               foreach ($stmt->fetchAll() as $row) {
+                $stmt = $con->prepare("select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.CategoryId='" . $_SESSION['catid'] . "' and tblposts.Is_Active=1 order by tblposts.id desc LIMIT $offset, $no_of_records_per_page");
+                $stmt->execute();
+                if ($stmt->rowCount() == 0) { {
+                        echo "No record found";
+                    }
+                } else {
+                    foreach ($stmt->fetchAll() as $row) {
 
 
-            ?>
-                <h1><?php echo htmlentities($row['category']); ?> News</h1>
-                <div class="card mb-4">
-                    <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']); ?>"
-                        alt="<?php echo htmlentities($row['posttitle']); ?>">
-                    <div class="card-body">
-                        <p class="text-right mb-0"><small>Posted on
-                                <?php echo htmlentities($row['postingdate']); ?></small></p>
+                ?>
+                        <h1><?php echo htmlentities($row['category']); ?> News</h1>
+                        <div class="card mb-4">
+                            <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']); ?>" alt="<?php echo htmlentities($row['posttitle']); ?>">
+                            <div class="card-body">
+                                <p class="text-right mb-0"><small>Posted on
+                                        <?php echo htmlentities($row['postingdate']); ?></small></p>
 
-                        <a href="news-details.php?nid=<?php echo htmlentities($row['pid']) ?>"
-                            class="card-title text-decoration-none text-dark">
-                            <h2 class="card-title text-dark"><?php echo htmlentities($row['posttitle']); ?></h2>
-                        </a>
+                                <a href="news-details.php?nid=<?php echo htmlentities($row['pid']) ?>" class="card-title text-decoration-none text-dark">
+                                    <h2 class="card-title text-dark"><?php echo htmlentities($row['posttitle']); ?></h2>
+                                </a>
 
-                        <a href="news-details.php?nid=<?php echo htmlentities($row['pid']) ?>" class="">Read More
-                            &rarr;</a>
-                    </div>
+                                <a href="news-details.php?nid=<?php echo htmlentities($row['pid']) ?>" class="">Read More
+                                    &rarr;</a>
+                            </div>
 
-                </div>
+                        </div>
+                    <?php } ?>
+
+                    <ul class="pagination justify-content-center mb-4">
+                        <li class="page-item"><a href="?pageno=1" class="page-link">First</a></li>
+                        <li class="<?php if ($pageno <= 1) {
+                                        echo 'disabled';
+                                    } ?> page-item">
+                            <a href="<?php if ($pageno <= 1) {
+                                            echo '#';
+                                        } else {
+                                            echo "?pageno=" . ($pageno - 1);
+                                        } ?>" class="page-link">Prev</a>
+                        </li>
+                        <li class="<?php if ($pageno >= $total_pages) {
+                                        echo 'disabled';
+                                    } ?> page-item">
+                            <a href="<?php if ($pageno >= $total_pages) {
+                                            echo '#';
+                                        } else {
+                                            echo "?pageno=" . ($pageno + 1);
+                                        } ?> " class="page-link">Next</a>
+                        </li>
+                        <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
+                    </ul>
                 <?php } ?>
-
-                <ul class="pagination justify-content-center mb-4">
-                    <li class="page-item"><a href="?pageno=1" class="page-link">First</a></li>
-                    <li class="<?php if ($pageno <= 1) {
-                                 echo 'disabled';
-                              } ?> page-item">
-                        <a href="<?php if ($pageno <= 1) {
-                                 echo '#';
-                              } else {
-                                 echo "?pageno=" . ($pageno - 1);
-                              } ?>" class="page-link">Prev</a>
-                    </li>
-                    <li class="<?php if ($pageno >= $total_pages) {
-                                 echo 'disabled';
-                              } ?> page-item">
-                        <a href="<?php if ($pageno >= $total_pages) {
-                                 echo '#';
-                              } else {
-                                 echo "?pageno=" . ($pageno + 1);
-                              } ?> " class="page-link">Next</a>
-                    </li>
-                    <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
-                </ul>
-                <?php } ?>
-
-
-
-
                 <!-- Pagination -->
-
-
-<<<<<<< HEAD
-        $stmt=$con->prepare("select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.CategoryId='".$_SESSION['catid']."' and tblposts.Is_Active=1 order by tblposts.id desc LIMIT $offset, $no_of_records_per_page"); 
-        $stmt->execute(); 
-        if ($stmt->rowCount()==0) {
-{
-echo "No record found";
-}}
-else {
-   foreach ($stmt->fetchAll() as $row)
-   {
-=======
->>>>>>> 01312192271f788e1d34b8071c20b22c97f13419
-
-
             </div>
 
             <!-- Sidebar Widgets Column -->
