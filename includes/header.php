b809673afbbecd65e1a441002b21098be0260e15
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2024-01-14 14:05:06 -0800

    delete donate

--- includes/header.php
+++ includes/header.php
@@ -1,37 +1,35 @@
-<div class="rtlas" align="right" >
-<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white fixed-top">
-   <div class="container">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-center" id="navbarResponsive">
-         <ul class="navbar-nav">
-            <li class="nav-item">
-               <a class="nav-link" href="index.php"><i class="fa fa-home"></i>الرئيسية</a>
-            </li>
-            <li class="nav-item">
-<a class="nav-link" href="donations.php"><i class="fa fa-leaf"></i> التبرعات</a>
+<div class="rtlas" align="right">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white fixed-top">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php"><i class="fa fa-home"></i>الرئيسية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php"><i class="fa-newspaper-o fa"></i> الأخبار</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact-us.php"><i class="fa fa-phone"></i> تواصل معنا</a>
+                    </li>
 
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="index.php"><i class="fa-newspaper-o fa"></i> الأخبار</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="contact-us.php"><i class="fa fa-phone"></i>  تواصل معنا</a>
-            </li>
+                    <li>
+                        <a class="nav-link" href="about-us.php"><i class="fa fa-info-circle"></i> حول الصفحة</a>
 
-            <li>
-               <a class="nav-link" href="about-us.php"><i class="fa fa-info-circle"></i> حول  الصفحة</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <div id="google_translate_element"></div>
+            </div>
+        </div>
+    </nav>
 
-            </li>
-         </ul>
-      </div>
-      <div>
-         <div id="google_translate_element"></div>
-      </div>
-   </div>
-</nav>
-
-<a href="index.php"><img height="120px" width="1200px" src="images/Quds.jpg" alt=""></a> 
-<img  width="1200px" src="images/header.jpg" alt="">
+    <a href="index.php"><img height="120px" width="1200px" src="images/Quds.jpg" alt=""></a>
+    <img width="1200px" src="images/header.jpg" alt="">
 </div>
