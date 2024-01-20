7c4dae87b942b94fbe8ba7a5680f9cab723003c9
77Ahmed <alainiahmed89@gmail.com>
2024-01-20 17:24:51 +0300

    styling of profile

--- admin/includes/topheader.php
+++ admin/includes/topheader.php
@@ -103,6 +103,8 @@
 
                                 <li><a href="change-password.php"><i class="ti-settings m-r-5"></i> Change Password</a>
                                 </li>
+                                <li><a href="editingProfile.php"><i class="ti-settings m-r-5"></i>Edit Profile</a>
+                                </li>
 
                                 <li><a href="logout.php"><i class="ti-power-off m-r-5"></i> Logout</a></li>
                             </ul>
