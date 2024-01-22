7d851463043f3274dcd4ddde51f10cf7078c5f3f
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2024-01-21 18:58:06 -0800

    add dote to the image name

--- admin/add-subadmins.php
+++ admin/add-subadmins.php
@@ -27,7 +27,7 @@ if (strlen($_SESSION['login']) == 0) {
          echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
       } else {
          //rename the image file
-         $imgnewfile = md5($imgfile) . $extension;
+         $imgnewfile = md5($imgfile) . '.' . $extension;
          // Code for move image into directory
          move_uploaded_file($_FILES["userigme"]["tmp_name"], "login/profilephoto/" . $imgnewfile);
          $stmt = $con->prepare("insert into tbladmin(AdminUserName,AdminEmailId,AdminPassword,userType,profile_imge ) values('$username','$email','$password','$usertype','$imgnewfile')");
@@ -44,87 +44,80 @@ if (strlen($_SESSION['login']) == 0) {
 ?>
 
 
-<?php include('includes/topheader.php'); ?>
-<!-- Top Bar End -->
-<!-- ========== Left Sidebar Start ========== -->
-<?php include('includes/leftsidebar.php'); ?>
-<!-- Left Sidebar End -->
-<div class="content-page">
-    <!-- Start content -->
-    <div class="content">
-        <div class="container">
+   <?php include('includes/topheader.php'); ?>
+   <!-- Top Bar End -->
+   <!-- ========== Left Sidebar Start ========== -->
+   <?php include('includes/leftsidebar.php'); ?>
+   <!-- Left Sidebar End -->
+   <div class="content-page">
+      <!-- Start content -->
+      <div class="content">
+         <div class="container">
             <div class="row">
-                <div class="col-xs-12">
-                    <div class="page-title-box">
-                        <h4 class="page-title">Add Subadmin</h4>
-                        <ol class="breadcrumb p-0 m-0">
-                            <li>
-                                <a href="#">Admin</a>
-                            </li>
-                            <li>
-                                <a href="#">Subadmin </a>
-                            </li>
-                            <li class="active">
-                                Add Subadmin
-                            </li>
-                        </ol>
-                        <div class="clearfix"></div>
-                    </div>
-                </div>
+               <div class="col-xs-12">
+                  <div class="page-title-box">
+                     <h4 class="page-title">Add Subadmin</h4>
+                     <ol class="breadcrumb p-0 m-0">
+                        <li>
+                           <a href="#">Admin</a>
+                        </li>
+                        <li>
+                           <a href="#">Subadmin </a>
+                        </li>
+                        <li class="active">
+                           Add Subadmin
+                        </li>
+                     </ol>
+                     <div class="clearfix"></div>
+                  </div>
+               </div>
             </div>
             <!-- end row -->
             <div class="row">
-                <div class="col-sm-12">
-                    <div class="card-box">
-                        <h4 class="m-t-0 header-title"><b>Add Subadmin </b></h4>
-                        <hr />
-                        <div class="row">
-                            <form class="row" name="addsuadmin" method="post" enctype="multipart/form-data">
-                                <div class="form-group col-md-6">
-                                    <label for="exampleInputusername">Username (used for login)</label>
-                                    <input type="text" placeholder="Enter Sub-Admin Username" name="sadminusername"
-                                        id="sadminusername" class="form-control"
-                                        pattern="^[a-zA-Z][a-zA-Z0-9-_.]{5,12}$"
-                                        title="Username must be alphanumeric 6 to 12 chars" onBlur="checkAvailability()"
-                                        required>
-                                    <span id="user-availability-status" style="font-size:14px;"></span>
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="emailid">Email Id</label>
-                                    <input type="email" class="form-control" id="emailid" name="emailid"
-                                        placeholder="Enter email" required>
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="password">Password</label>
-                                    <input type="password" class="form-control" id="pwd" name="pwd"
-                                        placeholder="Enter password" required>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <div class="card-box">
-                                            <h4 class="m-b-30 m-t-0 header-title"><b>profile Image</b></h4>
-                                            <input type="file" class="form-control" id="userigme" name="userigme">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                        <button type="submit" class="btn btn-custom waves-effect waves-light btn-md"
-                                            id="submit" name="submit">
-                                            Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+               <div class="col-sm-12">
+                  <div class="card-box">
+                     <h4 class="m-t-0 header-title"><b>Add Subadmin </b></h4>
+                     <hr />
+                     <div class="row">
+                        <form class="row" name="addsuadmin" method="post" enctype="multipart/form-data">
+                           <div class="form-group col-md-6">
+                              <label for="exampleInputusername">Username (used for login)</label>
+                              <input type="text" placeholder="Enter Sub-Admin Username" name="sadminusername" id="sadminusername" class="form-control" pattern="^[a-zA-Z][a-zA-Z0-9-_.]{5,12}$" title="Username must be alphanumeric 6 to 12 chars" onBlur="checkAvailability()" required>
+                              <span id="user-availability-status" style="font-size:14px;"></span>
+                           </div>
+                           <div class="form-group col-md-6">
+                              <label for="emailid">Email Id</label>
+                              <input type="email" class="form-control" id="emailid" name="emailid" placeholder="Enter email" required>
+                           </div>
+                           <div class="form-group col-md-6">
+                              <label for="password">Password</label>
+                              <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter password" required>
+                           </div>
+                           <div class="row">
+                              <div class="col-sm-12">
+                                 <div class="card-box">
+                                    <h4 class="m-b-30 m-t-0 header-title"><b>profile Image</b></h4>
+                                    <input type="file" class="form-control" id="userigme" name="userigme">
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="form-group">
+                              <div class="col-md-12">
+                                 <button type="submit" class="btn btn-custom waves-effect waves-light btn-md" id="submit" name="submit">
+                                    Submit</button>
+                              </div>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
             </div>
             <!-- end row -->
-        </div>
-        <!-- container -->
-    </div>
-    <!-- content -->
-    <?php include('includes/footer.php'); ?>
+         </div>
+         <!-- container -->
+      </div>
+      <!-- content -->
+      <?php include('includes/footer.php'); ?>
 
 
-    <?php } ?>
+   <?php } ?>
