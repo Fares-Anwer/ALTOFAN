1821d6aa86a1033954b1fae00ca813cdd7ca3942
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2024-01-21 16:23:23 -0800

    fix error on editing profile page

--- admin/editingProfile.php
+++ admin/editingProfile.php
@@ -2,14 +2,10 @@
 session_start();
 include('includes/config.php');
 error_reporting(0);
-if(strlen($_SESSION['login'])==0){ 
+if (strlen($_SESSION['login']) == 0) {
     header('location:index.php');
-}else{
-    if(isset($_POST['button'])){
-
-
-     
-
+} else {
+    if (isset($_POST['button'])) {
     }
 }
 ?>
@@ -21,153 +17,170 @@ if(strlen($_SESSION['login'])==0){
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <style type="text/css">
-    	body{margin-top:20px;
-background-color:#f2f6fc;
-color:#69707a;
-}
-.img-account-profile {
-    height: 10rem;
-}
-.rounded-circle {
-    border-radius: 50% !important;
-}
-.card {
-    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-}
-.card .card-header {
-    font-weight: 500;
-}
-.card-header:first-child {
-    border-radius: 0.35rem 0.35rem 0 0;
-}
-.card-header {
-    padding: 1rem 1.35rem;
-    margin-bottom: 0;
-    background-color: rgba(33, 40, 50, 0.03);
-    border-bottom: 1px solid rgba(33, 40, 50, 0.125);
-}
-.form-control, .dataTable-input {
-    display: grid;
-    width: 100%;
-    padding: 0.875rem 1.125rem;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1;
-    color: #69707a;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #c5ccd6;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    border-radius: 0.35rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
+    body {
+        margin-top: 20px;
+        background-color: #f2f6fc;
+        color: #69707a;
+    }
 
-.nav-borders .nav-link.active {
-    color: #0061f2;
-    border-bottom-color: #0061f2;
-}
-.nav-borders .nav-link {
-    color: #69707a;
-    border-bottom-width: 0.125rem;
-    border-bottom-style: solid;
-    border-bottom-color: transparent;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: 1rem;
-    margin-right: 1rem;
-}
-    </style>
+    .img-account-profile {
+        height: 10rem;
+    }
+
+    .rounded-circle {
+        border-radius: 50% !important;
+    }
+
+    .card {
+        box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
+    }
+
+    .card .card-header {
+        font-weight: 500;
+    }
+
+    .card-header:first-child {
+        border-radius: 0.35rem 0.35rem 0 0;
+    }
+
+    .card-header {
+        padding: 1rem 1.35rem;
+        margin-bottom: 0;
+        background-color: rgba(33, 40, 50, 0.03);
+        border-bottom: 1px solid rgba(33, 40, 50, 0.125);
+    }
+
+    .form-control,
+    .dataTable-input {
+        display: grid;
+        width: 100%;
+        padding: 0.875rem 1.125rem;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1;
+        color: #69707a;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #c5ccd6;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        border-radius: 0.35rem;
+        transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    }
+
+    .nav-borders .nav-link.active {
+        color: #0061f2;
+        border-bottom-color: #0061f2;
+    }
+
+    .nav-borders .nav-link {
+        color: #69707a;
+        border-bottom-width: 0.125rem;
+        border-bottom-style: solid;
+        border-bottom-color: transparent;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        padding-left: 0;
+        padding-right: 0;
+        margin-left: 1rem;
+        margin-right: 1rem;
+    }
+</style>
 </head>
+
 <body>
-<div class="container-xl px-4 mt-4">
+    <div class="container-xl px-4 mt-4">
 
-<hr class="mt-0 mb-4">
-<div class="row">
-<div class="col-xl-4">
+        <hr class="mt-0 mb-4">
+        <div class="row">
+            <div class="col-xl-4">
 
-<div class="card mb-4 mb-xl-0">
-<div class="card-header">Profile Picture</div>
-<div class="card-body text-center">
+                <div class="card mb-4 mb-xl-0">
+                    <div class="card-header">Profile Picture</div>
+                    <div class="card-body text-center">
 
-<img class="img-account-profile rounded-circle mb-2" src="assets/images/users/170.jpg" alt>
+                        <img class="img-account-profile rounded-circle mb-2" src="assets/images/users/170.jpg" alt>
 
-<div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
 
-<button class="btn btn-primary" type="button" ><form action="">
-    <input type="file">
-</form></button>
-</div>
-</div>
-</div>
-<div class="col-xl-8">
+                        <button class="btn btn-primary" type="button">
+                            <form action="">
+                                <input type="file">
+                            </form>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-8">
 
-<div class="card mb-4">
-<div class="card-header">Account Details</div>
-<div class="card-body">
-     <?php 
-                              $aid=intval($_GET['said']);
-                              $stmt=$con->prepare("Select * from  tbladmin where userType=0 && id='$aid'"); 
-                              $stmt->execute(); 
-                            $cnt=1;
+                <div class="card mb-4">
+                    <div class="card-header">Account Details</div>
+                    <div class="card-body">
+                        <?php
+                        $aid = intval($_GET['said']);
+                        $stmt = $con->prepare("Select * from  tbladmin where userType=0 && id='$aid'");
+                        $stmt->execute();
+                        $cnt = 1;
                         if ($stmt->rowCount()) {
-                             foreach ($stmt->fetchAll() as $row)
-                              {
-                              ?>
-<form name="login">
+                            foreach ($stmt->fetchAll() as $row) {
+                        ?>
+                                <form name="login">
 
-<div class="mb-3">
-<label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-<input class="form-control" id="inputUsername" type="text" value=<?php echo htmlentities($row['AdminUserName']);?>>
-</div>
+                                    <div class="mb-3">
+                                        <label class="small mb-1" for="inputUsername">Username (how your name will appear to
+                                            other users on the site)</label>
+                                        <input class="form-control" id="inputUsername" type="text" value=<?php echo htmlentities($row['AdminUserName']); ?>>
+                                    </div>
 
-<div class="row gx-3 mb-3">
+                                    <div class="row gx-3 mb-3">
 
-<div class="col-md-6">
-<label class="small mb-1" for="inputFirstName">First name</label>
-<input class="form-control" id="inputFirstName" type="text" value=<?php echo htmlentities($row['AdminUserName']);?>>
-</div>
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputFirstName">First name</label>
+                                            <input class="form-control" id="inputFirstName" type="text" value=<?php echo htmlentities($row['AdminUserName']); ?>>
+                                        </div>
 
-<div class="col-md-6">
-<label class="small mb-1" for="inputLastName">Last name</label>
-<input class="form-control" id="inputLastName" type="text"  value=<?php echo htmlentities($row['AdminEmailId']);?>>
-</div>
-</div>
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputLastName">Last name</label>
+                                            <input class="form-control" id="inputLastName" type="text" value=<?php echo htmlentities($row['AdminEmailId']); ?>>
+                                        </div>
+                                    </div>
 
-<div class="row gx-3 mb-3">
-
-
-
-
-<div class="mb-3">
-<label class="small mb-1" for="inputEmailAddress">Email address</label>
-<input class="form-control" id="inputEmailAddress" type="email"value="name@example.com">
-</div>
-
-<div class="row gx-3 mb-3">
-
-<div class="col-md-6">
-<label class="small mb-1" for="inputPhone">Phone number</label>
-<input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="+967 ">
-</div>
+                                    <div class="row gx-3 mb-3">
 
 
 
-<button class="btn btn-primary" type="button" style="margin: 5px;">Save changes</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript">
-	
-</script>
+
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                                            <input class="form-control" id="inputEmailAddress" type="email" value="name@example.com">
+                                        </div>
+
+                                        <div class="row gx-3 mb-3">
+
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputPhone">Phone number</label>
+                                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="+967 ">
+                                            </div>
+
+
+
+                                            <button class="btn btn-primary" type="button" style="margin: 5px;">Save
+                                                changes</button>
+                                </form>
+                        <?php }
+                        } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+
+    </script>
 </body>
+<?php include('includes/footer.php'); ?>
+
 </html>
-<?php include('includes/footer.php');?>
